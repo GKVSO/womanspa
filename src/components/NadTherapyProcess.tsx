cd6eb4b0d61c3fe12bd0685aff4ed7e674593cdc
GKVSO <itsergey1001@gmail.com>
2026-09-01 19:04:37 +0500

    fix:adaptive: TherapyProcess section into Nad+ page

--- src/components/NadTherapyProcess.tsx
+++ src/components/NadTherapyProcess.tsx
@@ -40,7 +40,7 @@ function Divider() {
 export default function NadTherapyProcess() {
   const t = useT();
   return (
-    <section className="relative overflow-hidden px-5 sm:px-10 pt-16 sm:pt-20 pb-16 sm:pb-24 my-15 my:mt-25 lg:my-30 bg-[#eae8eb] rounded-[60px] bg-cover bg-right min-[1200px]:bg-center bg-[url(/nad-process-bg.png)] bg-no-repeat">
+    <section className="relative overflow-hidden px-5 sm:px-10 pt-16 sm:pt-20 pb-16 sm:pb-24 my-15 my:mt-25 lg:my-30 bg-[#eae8eb] rounded-[60px] bg-cover bg-[70%] lg:bg-[50%] min-[1200px]:bg-center bg-[url(/nad-process-bg.png)] bg-no-repeat">
       <div className="flex flex-col">
         <StaggerContainer staggerDelay={0.12} className="max-w-[760px]">
           <StaggerItem>
