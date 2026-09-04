8e8dfc219572de5487bba3458675edf01cb15674
GKVSO <itsergey1001@gmail.com>
2026-09-04 18:54:11 +0500

    fix:adaptive: bg image for process section on iv and nad pages

--- src/components/IvTherapyProcess.tsx
+++ src/components/IvTherapyProcess.tsx
@@ -40,7 +40,7 @@ function Divider() {
 export default function IvTherapyProcess() {
   const t = useT();
   return (
-    <section className="relative overflow-hidden px-5 sm:px-10 pt-16 sm:pt-20 pb-16 sm:pb-24 my-15 my:mt-25 lg:my-30 bg-[#edecee] rounded-[60px] bg-position-[right_-400px_bottom] min-[1200px]:bg-bottom-center bg-[url(/iv-process-bg.png)] bg-no-repeat">
+    <section className="relative overflow-hidden px-5 sm:px-10 pt-16 sm:pt-20 my-15 my:mt-25 lg:my-30 bg-[#edecee] rounded-[60px] bg-position-[right_-400px_bottom] min-[1200px]:bg-bottom-center md:bg-[url(/iv-process-bg.png)] bg-no-repeat">
       <div className="flex flex-col">
         <StaggerContainer staggerDelay={0.12} className="max-w-[760px]">
           <StaggerItem>
@@ -73,6 +73,9 @@ export default function IvTherapyProcess() {
             </Fragment>
           ))}
         </StaggerContainer>
+      </div>
+      <div className="flex md:hidden justify-center">
+        <img src="/iv-process.png" alt="IV therapy session in a luxury wellness setting." />
       </div>
     </section>
   );
