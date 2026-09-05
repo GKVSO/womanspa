37f0bb155c3ba074644b2ceaf5e5388e28395b36
GKVSO <itsergey1001@gmail.com>
2026-09-05 16:31:04 +0500

    fix: button with 2 lines on Sylfirm page

--- src/components/SylfirmCandidate.tsx
+++ src/components/SylfirmCandidate.tsx
@@ -33,8 +33,8 @@ export default function SylfirmCandidate() {
       <div className="flex justify-center mt-[20px] md:mt-[50px] 2xl:absolute left-[8%] bottom-[10%] z-20">
         <FadeIn as="div" y={30}>
           <div className="bg-white/80 rounded-[30px] p-6 sm:p-10 w-full sm:w-[620px] backdrop-blur-sm">
-            <div className="flex flex-col lg:flex-row justify-between items-stretch lg:items-end gap-6 lg:gap-10">
-              <div>
+            <div className="flex flex-col flex-wrap lg:flex-row justify-between items-stretch lg:items-end gap-6">
+              <div className="basis-[57%] grow">
                 <h3 className="text-[#313242] font-bold text-[20px] min-[768px]:text-[32px] mb-6">
                   {t("Ideal Candidate List")}
                 </h3>
@@ -57,7 +57,7 @@ export default function SylfirmCandidate() {
                   ))}
                 </ul>
               </div>
-              <BookButton className="mt-6" style={{ backgroundColor: "#B07E3F" }}>
+              <BookButton className="mt-6 whitespace-nowrap" style={{ backgroundColor: "#B07E3F" }}>
                 {t("Book Consultation")}
                 </BookButton>
             </div>
