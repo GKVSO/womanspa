4964c569b044da84dc3f53878db26b808b152603
GKVSO <itsergey1001@gmail.com>
2026-09-06 21:08:11 +0500

    fix: 2 lines on Primelase page

--- src/components/PrimelaseCandidate.tsx
+++ src/components/PrimelaseCandidate.tsx
@@ -36,8 +36,8 @@ export default function PrimelaseCandidate() {
       <div className="relative z-20 mt-12 sm:mt-48 mr-auto w-full sm:w-fit">
         <FadeIn as="div" y={30}>
           <div className="bg-white/80 rounded-[30px] p-6 sm:p-10 w-full sm:w-[620px] backdrop-blur-sm">
-            <div className="flex flex-col lg:flex-row justify-between items-stretch lg:items-end gap-6 lg:gap-10">
-              <div>
+            <div className="flex flex-col flex-wrap lg:flex-row justify-between items-stretch lg:items-end gap-6">
+              <div className="basis-[57%] grow">
                 <h3 className="text-[#313242] font-bold text-[20px] min-[768px]:text-[32px] mb-6">
                   {t("Ideal Candidate List")}
                 </h3>
@@ -78,7 +78,7 @@ export default function PrimelaseCandidate() {
                   ))}
                 </ul>
               </div>
-              <BookButton className="mt-6" style={{ backgroundColor: "#B07E3F" }}>
+              <BookButton className="mt-6 whitespace-nowrap" style={{ backgroundColor: "#B07E3F" }}>
                 {t("Book Consultation")}
                 </BookButton>
             </div>
