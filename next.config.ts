6a123de91812c70bf8e427bd84cf040e2bfb45ea
GKVSO <itsergey1001@gmail.com>
2026-09-10 18:53:08 +0500

    config: standalone

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  output: "standalone",
   images: {
     // unoptimized: true removed to enable Next.js native optimization
   },
