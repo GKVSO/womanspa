a3c3c443df2f6bedc5b36cf50f320002ee101fac
GKVSO <itsergey1001@gmail.com>
2026-09-12 20:20:15 +0500

    fix secrets

--- check_page.js
+++
@@ -1,8 +0,0 @@
-const postgres = require('postgres');
-const sql = postgres('postgresql://neondb_owner:npg_aescCOAz26qb@ep-misty-union-b2nepbs9.c-6.eu-central-1.aws.neon.tech/neondb?sslmode=require&channel_binding=require', { max: 1 });
-async function run() {
-  const page = await sql`SELECT id, slug FROM pages WHERE slug = 'exion'`;
-  console.log(page);
-  process.exit(0);
-}
-run();
--- dump.js
+++
@@ -1,8 +0,0 @@
-const postgres = require('postgres');
-const sql = postgres('postgresql://neondb_owner:npg_aescCOAz26qb@ep-misty-union-b2nepbs9.c-6.eu-central-1.aws.neon.tech/neondb?sslmode=require&channel_binding=require', { max: 1 });
-async function run() {
-  const blocks = await sql`SELECT content FROM page_blocks LIMIT 10`;
-  console.log(JSON.stringify(blocks, null, 2));
-  process.exit(0);
-}
-run();
